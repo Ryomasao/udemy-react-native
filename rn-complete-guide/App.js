596010dcfe43bd8b143ce17346a54d864b529f11
Ryomasao <konoemario@gmail.com>
2020-01-12 13:04:04 +0900

    TODOリストのADDとDELETEを実装

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [enterdGoal, setEnteredGoal] = useState("");
+  const [couseGoals, setCourseGoal] = useState([]);
+
+  const goalInputHandler = enterdGoal => {
+    setEnteredGoal(enterdGoal);
+  };
+
+  const addGoalHandelr = () => {
+    setCourseGoal(currentGoals => [...currentGoals, enterdGoal]);
+    setEnteredGoal("");
+  };
+
+  const deleteGoalHandler = deleteTargetIndex => {
+    const newGoals = couseGoals.filter(
+      (_, index) => index !== deleteTargetIndex
+    );
+    setCourseGoal(newGoals);
+  };
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Course Goal" style={styles.input} />
-        <Button title="ADD" />
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+          value={enterdGoal}
+          onChangeText={goalInputHandler}
+        />
+        <Button title="ADD" onPress={addGoalHandelr} />
+      </View>
+      <View>
+        {couseGoals.map((goal, index) => (
+          <View key={index} style={styles.listItem}>
+            <Text>{goal}</Text>
+            <Button title="×" onPress={() => deleteGoalHandler(index)} />
+          </View>
+        ))}
       </View>
     </View>
   );
@@ -22,8 +54,19 @@ const styles = StyleSheet.create({
     alignItems: "center"
   },
   input: {
+    width: "80%",
     borderColor: "black",
     borderWidth: 1,
     padding: 10
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
   }
 });
