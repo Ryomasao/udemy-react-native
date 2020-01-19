b3a9ed86200a5eff18f33c9543ea813843f1dda0
Ryomasao <konoemario@gmail.com>
2020-01-19 19:51:47 +0900

    数字の履歴を表示する

--- number-game/screens/GameScreen.js
+++ number-game/screens/GameScreen.js
@@ -1,11 +1,12 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { View, Text, StyleSheet, Alert } from 'react-native'
+import { View, Text, StyleSheet, Alert, ScrollView } from 'react-native'
 // アイコンも提供してる。すごい。
 import { Ionicons } from '@expo/vector-icons'
 
 import NumberContainer from '../components/NumberContainer'
 import Card from '../components/Card'
 import MainButton from '../components/MainButton'
+import BodyText from '../components/BodyText'
 
 const generateRandomBetween = (min, max, exclude) => {
   // 切り上げ
@@ -20,19 +21,24 @@ const generateRandomBetween = (min, max, exclude) => {
   }
 }
 
-const GameScreen = ({ userChoice, onGameOver }) => {
-  const [currentGuess, setCurrentGuess] = useState(
-    generateRandomBetween(1, 100, userChoice)
-  )
+const renderListItem = (value, numOfRound) => (
+  <View key={value} style={styles.listItem}>
+    <BodyText>#{numOfRound}</BodyText>
+    <BodyText>{value}</BodyText>
+  </View>
+)
 
-  const [rounds, setRounds] = useState(0)
+const GameScreen = ({ userChoice, onGameOver }) => {
+  const initialGuess = generateRandomBetween(1, 100, userChoice)
+  const [currentGuess, setCurrentGuess] = useState(initialGuess)
+  const [pastGuesses, setPastGuess] = useState([initialGuess])
 
   const currentLow = useRef(1)
   const currentHigh = useRef(100)
 
   useEffect(() => {
     if (currentGuess === userChoice) {
-      onGameOver(rounds)
+      onGameOver(pastGuesses.length)
     }
   }, [currentGuess, userChoice, onGameOver])
 
@@ -50,17 +56,20 @@ const GameScreen = ({ userChoice, onGameOver }) => {
     if (direction === 'lower') {
       currentHigh.current = currentGuess
     } else {
-      currentLow.current = currentGuess
+      // Listのkeyに指定した
+      // + 1しないロジックだとminが重複することがあるのかしら
+      currentLow.current = currentGuess + 1
     }
-    setCurrentGuess(
-      generateRandomBetween(
-        currentLow.current,
-        currentHigh.current,
-        currentGuess
-      )
+
+    const nextNumber = generateRandomBetween(
+      currentLow.current,
+      currentHigh.current,
+      currentGuess
     )
 
-    setRounds(currentRounds => currentRounds + 1)
+    setCurrentGuess(nextNumber)
+
+    setPastGuess(pastGuesses => [nextNumber, ...pastGuesses])
   }
 
   return (
@@ -75,6 +84,13 @@ const GameScreen = ({ userChoice, onGameOver }) => {
           <Ionicons name="md-add" size={24} color="white" />
         </MainButton>
       </Card>
+      <View style={styles.listContainer}>
+        <ScrollView contentContainerStyle={styles.list}>
+          {pastGuesses.map((guess, index) =>
+            renderListItem(guess, pastGuesses.length - index)
+          )}
+        </ScrollView>
+      </View>
     </View>
   )
 }
@@ -91,6 +107,29 @@ const styles = StyleSheet.create({
     marginTop: 20,
     width: 400,
     maxWidth: '80%',
+  },
+  listContainer: {
+    width: '80%',
+    // androidだと以下が必要
+    flex: 1,
+  },
+  // scrollViewにstyleをあてるにはcontentContainerStyleを使う
+  list: {
+    // itemが多い場合にスクロールさせたい
+    //flexGrow: 1,
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  listItem: {
+    borderColor: '#ccc',
+    borderWidth: 1,
+    padding: 15,
+    marginVertical: 10,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '60%',
   },
 })
 
